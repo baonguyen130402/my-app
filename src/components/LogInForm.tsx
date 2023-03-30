35b47db77ba91fb01a7eb991ca251cb84e346731
imsumoregn <87747324+imsumoregn@users.noreply.github.com>
2023-03-30 20:06:10 +0700

    feat(auth): refine `logIn`

--- src/components/LogInForm.tsx
+++ src/components/LogInForm.tsx
@@ -22,18 +22,18 @@ export const LogInForm = ({ setAuth }: { setAuth: () => void }) => {
   };
 
   const [formValues, setFormValues] = useForm(initialValue);
+  const [errorMessage, setErrorMessage] = useState("");
 
   return (
     <div className="fixed top-0 left-0 bottom-0 right-0 flex h-screen flex-col items-center justify-center bg-gray-500">
       <div className="w-6/12 rounded-xl bg-white p-5 text-center text-black">
         <h1 className="mb-3 text-3xl font-bold">Login</h1>
 
-        <form
-          onSubmit={() => {
-            alert(formValues);
-          }}
-        >
+        <form >
           <div className="mb-3 flex flex-col items-start">
+            {errorMessage && <h1>{`Error: ${errorMessage}`}</h1>}
+
+
             <label className="text-md mb-1 font-semibold">Email:</label>
             <input
               className="focus:border-1 mb-2 w-full rounded-lg border border-gray-200 p-1 focus:mb-2 focus:rounded-lg focus:border-transparent focus:bg-gray-200 focus:outline-none"
@@ -58,12 +58,23 @@ export const LogInForm = ({ setAuth }: { setAuth: () => void }) => {
           <button
             className="text mt-3 w-full rounded-xl bg-blue-600 px-4 py-2 text-stone-300 hover:bg-blue-500"
             type="button"
-            onClick={(event) => {
-              // AuthService.logIn(formValues);
-              alert(
-                `Username: ${formValues.username} 
-Password: ${formValues.password}`
-              );
+            onClick={async event => {
+              try {
+                await AuthService.logIn(...formValues);
+              } catch (error) {
+                // if (typeof error === "string") {
+                //   setErrorMessage(error)
+                // } else if (
+                //   typeof error === "object"
+                //   && typeof Object.hasOwn(error, "message")
+                //   && typeof error.message === "string"
+                // ) {
+                //   setErrorMessage(error.message)
+                // } else {
+                //   setErrorMessage("Something went wrong.")
+                // }
+                setErrorMessage((error as Record<"message", string>).message)
+              }
             }}
           >
             Login
--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -1,9 +1,18 @@
-const initialState = false;
+const initialState = !!(
+  typeof window === "object" && localStorage.getItem("state")
+)
 
 let isAuthenticated = initialState;
 
-const setIsAuthenticated = (newState: boolean) => (isAuthenticated = newState);
+const setIsAuthenticated = (newState: boolean) => isAuthenticated = newState;
 
+export class AuthError extends Error {
+  constructor(action: "LOG_IN" | "LOG_OUT", error: any) {
+    if (typeof error === "string") {
+      super(error)
+    } else super("Failed to log in.")
+  }
+}
 
 /**
  * Send a post request to the server.
@@ -11,14 +20,25 @@ const setIsAuthenticated = (newState: boolean) => (isAuthenticated = newState);
  * If the request succeeded, log the user in.
  * If failed, show error indicate what went wrong.
  */
-const logIn = (username: string, password: string): void => {
-  setIsAuthenticated(true);
-  localStorage.setItem("state", "true");
+const logIn = async (username: string, password: string): Promise<void> => {
+  try {
+    // POST credentials to the server
+    // const requestLogInResponse = await fetch("/api/auth", {
+    //   method: "POST",
+    //   // Request body must be serialisable.
+    //   body: JSON.stringify({ username, password })
+    // })
+
+    setIsAuthenticated(true);
+    localStorage.setItem("state", "true");
+  } catch (error) {
+    throw new AuthError("LOG_IN", error)
+  }
 };
 
 const logOut = () => {
   setIsAuthenticated(false);
-  localStorage.setItem("state", "false");
+  localStorage.removeItem("state");
 };
 
 const AuthService = {
